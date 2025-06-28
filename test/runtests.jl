42fde8d4e33316164fae6cc1cf0d364a7f385e78
Eiad Hamwi <eh652@cornell.edu>
2025-06-28 17:09:20 -0400

    Squashed commit of the following:
    commit 458c394b610a3d8d81e4956393c575852e6cca86
    Author: Eiad Hamwi <eh652@cornell.edu>
    Date:   Wed Jun 25 10:07:49 2025 -0400
    
        pulled new `@eles`
    
    commit f04a6b8520a2137060ee8000ea8f98ff9437c397
    Merge: e83bcd1 f6a5369
    Author: Eiad Hamwi <66403062+eiad-hamwi@users.noreply.github.com>
    Date:   Tue Jun 24 18:15:17 2025 -0400
    
        Merge branch 'main' into rename_duplicates
    
    commit e83bcd1f7fa8bba7030f426ace58b3ace626f9b4
    Author: Eiad Hamwi <eh652@cornell.edu>
    Date:   Tue Jun 24 18:08:45 2025 -0400
    
        Added & fixed tests
    
    commit 3e8902b3ae171323c89de67a58ec1ed847826a20
    Author: Eiad Hamwi <eh652@cornell.edu>
    Date:   Tue Jun 24 17:33:15 2025 -0400
    
        Make global variables for duplicates
        Check `isnothing(unique_name_suffix)`
        Set original variable to nothing
    
    commit ac1399d7a4b7ff69dbfcff131a7efbd6e42481e5
    Author: Eiad Hamwi <eh652@cornell.edu>
    Date:   Tue Jun 24 03:49:42 2025 -0400
    
        test `@ele` with `deepcopy_no_beamline`
    
    commit c6a2fb83c68393883507c8793d601d050dd3fea2
    Author: Eiad Hamwi <eh652@cornell.edu>
    Date:   Tue Jun 24 03:04:55 2025 -0400
    
        Added tests
        Removed macros from `Beamline` constructor
    
    commit fc31ca2ed0ba709f28dfe447206af935951df095
    Author: Eiad Hamwi <eh652@cornell.edu>
    Date:   Tue Jun 24 02:44:06 2025 -0400
    
        Automatically replace duplicate elements using `deepcopy_no_beamline` with different global variable names
    
    commit 0b5044b831ede5a263a9cc80aba25500a5462c37
    Author: Eiad Hamwi <eh652@cornell.edu>
    Date:   Tue Jun 24 01:57:05 2025 -0400
    
        `@ele` macro accepts `deepcopy_no_beamline` constructors and is more safe
    
    commit ec49e1cb035f6ff512a217789e3d0132f9973904
    Author: Eiad Hamwi <eh652@cornell.edu>
    Date:   Tue Jun 24 01:26:16 2025 -0400
    
        Naming macros `@ele` & `@eles`

--- test/runtests.jl
+++ test/runtests.jl
@@ -482,7 +482,42 @@ using Test
     @test typeof(ele.dz_rot) == ComplexF64
     @test !(ele.PatchParams === pp)
 
-    # @eles
+
+    # Test @eles macros
+    @eles tester = LineElement(class="SBend", L=3.0, g=0.1)
+    @eles quaddy = Quadrupole(L=0.20, K1=0.11)
+    @eval @eles quaddy2 = $(deepcopy_no_beamline(quaddy))
+    @test tester.name  == "tester"
+    @test quaddy.name  == "quaddy"
+    @test quaddy2.name == "quaddy2"
+
+    global drift1, drift2, solenoid, quadrupole
+    @eles begin
+      drift1 = Drift(L=0.5)
+      drift2 = Drift(L=0.75)
+      solenoid = Solenoid(L=0.25, Ks=0.1)
+      quadrupole = Quadrupole(L=0.15, K1=0.2)
+    end
+    @test drift1.name == "drift1"
+    @test drift2.name == "drift2"
+    @test solenoid.name == "solenoid"
+    @test quadrupole.name == "quadrupole"
+
+
+    # Test duplicate removal in Beamline
+    bl = Beamline([drift1, drift2, solenoid, drift1, 
+                   drift1, drift2, solenoid, quadrupole], Brho_ref=60.0, unique_name_suffix="_")
+    @test bl.line[1].name == "drift1_1"    && drift1_1    === bl.line[1]
+    @test bl.line[4].name == "drift1_2"    && drift1_2    === bl.line[4]
+    @test bl.line[5].name == "drift1_3"    && drift1_3    === bl.line[5]
+    @test bl.line[2].name == "drift2_1"    && drift2_1    === bl.line[2]
+    @test bl.line[6].name == "drift2_2"    && drift2_2    === bl.line[6]
+    @test bl.line[3].name == "solenoid_1"  && solenoid_1  === bl.line[3]
+    @test bl.line[7].name == "solenoid_2"  && solenoid_2  === bl.line[7]
+    @test bl.line[8].name == "quadrupole"  && quadrupole  === bl.line[8]
+
+
+  # @eles
     @eles qf = Quadrupole(K1=0.36)
     @test qf.name == "qf"
     @eles d = Drift()
